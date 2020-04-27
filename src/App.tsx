e7a240c026da2887b4fd987555e1724698f92e25
CedricPradels <cedric.pradels@gmail.com>
2020-04-27 10:01:03 +0200

    Add clues

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { maxHeaderSize } from "http";
 const size = "30";
 const dim = 20;
 
+const neighbourhood4 = [
+  [0, -1],
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+];
+
 function App() {
   const createCell = useCallback(
     () => ({
       isBomb: false,
       isFalgged: false,
       isReaveled: false,
+      clue: 0,
     }),
     []
   );
@@ -17,6 +25,43 @@ function App() {
     (dim) => Array.from(Array(dim), () => Array.from(Array(dim), createCell)),
     []
   );
+  const addClues = useCallback((setGrid) => {
+    const neighbourhood8 = [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [-1, 0],
+      [1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ];
+    setGrid((grid: any) => {
+      let swapGrid = [...grid];
+      const rows = grid.length;
+      const cols = grid[0].length;
+      swapGrid = grid.map((row: any, x: number) =>
+        row.map((cell: any, y: number) => {
+          let count = 0;
+          neighbourhood8.forEach(([dx, dy]) => {
+            const shiftedX = x + dx,
+              shiftedY = y + dy;
+            if (
+              shiftedX >= 0 &&
+              shiftedX < rows &&
+              shiftedY >= 0 &&
+              shiftedY < cols
+            ) {
+              if (!cell.isBomb && grid[x + dx][y + dy].isBomb) count++;
+            }
+          });
+          return { ...cell, clue: count };
+        })
+      );
+      return swapGrid;
+    });
+  }, []);
+
   const addBombs = useCallback((bombs, setGrid) => {
     const randomTo = (max: number) => Math.floor(Math.random() * max);
     setGrid((grid: any) => {
@@ -47,6 +92,13 @@ function App() {
       >
         Add bombs
       </button>
+      <button
+        onClick={() => {
+          addClues(setGrid);
+        }}
+      >
+        Add clues
+      </button>
       <div
         className="App"
         style={{
@@ -63,8 +115,13 @@ function App() {
                 backgroundColor: cell.isBomb ? "red" : "gray",
                 width: size + "px",
                 height: size + "px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
-            ></div>
+            >
+              {!!cell.clue ? cell.clue : ""}
+            </div>
           ))
         )}
       </div>
