01876b7460da53819bec3e89a9b2ff4c833cce34
CedricPradels <cedric.pradels@gmail.com>
2020-04-27 09:26:56 +0200

    Create bombs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from "react";
+import { maxHeaderSize } from "http";
 
 const size = "30";
 const dim = 20;
@@ -16,33 +17,58 @@ function App() {
     (dim) => Array.from(Array(dim), () => Array.from(Array(dim), createCell)),
     []
   );
-  const addBombs = useCallback((bombs, grid) => {}, []);
+  const addBombs = useCallback((bombs, setGrid) => {
+    const randomTo = (max: number) => Math.floor(Math.random() * max);
+    setGrid((grid: any) => {
+      const rows = grid.length;
+      const cols = grid[0].length;
+      const swapGrid = [...grid];
+      for (let i = 0; i < bombs; i++) {
+        let xBomb, yBomb;
+        do {
+          xBomb = randomTo(rows);
+          yBomb = randomTo(cols);
+        } while (swapGrid[xBomb][yBomb].isBomb);
+        swapGrid[xBomb][yBomb].isBomb = true;
+      }
+      return swapGrid;
+    });
+  }, []);
 
   const [grid, setGrid] = useState(createGrid(dim));
 
-  console.log(grid);
-
   return (
-    <div
-      className="App"
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${dim}, ${size}px)`,
-      }}
-    >
-      {grid.map((row, x) =>
-        row.map((cell, y) => (
-          <div
-            style={{
-              border: "1px black solid",
-              backgroundColor: "gray",
-              width: size + "px",
-              height: size + "px",
-            }}
-          ></div>
-        ))
-      )}
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setGrid(createGrid(dim));
+          addBombs(10, setGrid);
+        }}
+      >
+        Add bombs
+      </button>
+      <div
+        className="App"
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${dim}, ${size}px)`,
+        }}
+      >
+        {grid.map((row, x) =>
+          row.map((cell, y) => (
+            <div
+              key={`${x}${y}`}
+              style={{
+                border: "1px black solid",
+                backgroundColor: cell.isBomb ? "red" : "gray",
+                width: size + "px",
+                height: size + "px",
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
