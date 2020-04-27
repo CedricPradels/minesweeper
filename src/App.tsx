389585573277b16269d258a5b3076b2466c7a0f3
CedricPradels <cedric.pradels@gmail.com>
2020-04-27 09:07:18 +0200

    create grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+
+const size = "30";
+const dim = 20;
 
 function App() {
+  const createCell = useCallback(
+    () => ({
+      isBomb: false,
+      isFalgged: false,
+      isReaveled: false,
+    }),
+    []
+  );
+  const createGrid = useCallback(
+    (dim) => Array.from(Array(dim), () => Array.from(Array(dim), createCell)),
+    []
+  );
+  const addBombs = useCallback((bombs, grid) => {}, []);
+
+  const [grid, setGrid] = useState(createGrid(dim));
+
+  console.log(grid);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${dim}, ${size}px)`,
+      }}
+    >
+      {grid.map((row, x) =>
+        row.map((cell, y) => (
+          <div
+            style={{
+              border: "1px black solid",
+              backgroundColor: "gray",
+              width: size + "px",
+              height: size + "px",
+            }}
+          ></div>
+        ))
+      )}
     </div>
   );
 }
